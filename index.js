15df9174e645cedaef4cdcd48abe6f5fa4efd39b
Tony Hart <tonyzoof@gmail.com>
2021-04-23 15:21:14 -0400

    enable cache

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const serveStatic = require('serve-static');
 
 const app = express();
-app.use(serveStatic(path.join(__dirname, 'web-build')));
+app.use(serveStatic(path.join(__dirname, 'web-build'), { maxAge: 24 * 60 * 1000 }));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'web-build/index.html'));
 });
