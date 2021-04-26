61522284f586d9f24fb727c29d0a8950d8cdc716
Tony Hart <tonyzoof@gmail.com>
2021-04-26 10:52:44 -0400

    remove red bg

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -73,7 +73,7 @@ export default StyleSheet.create({
     justifyContent: 'center',
   },
   tint: { color: tintColorLight },
-  link: { width: '100%', backgroundColor: 'red' },
+  link: { width: '100%' },
 });
 
 export const headerStyle: StackNavigationOptions = {
